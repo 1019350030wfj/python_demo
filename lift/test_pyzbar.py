f9e93072cd8cdfa7133b9e095aaf171701989693
jayden <jayden@qq.com>
2017-11-21 18:12:05 +0800

    movieUrls

--- lift/test_pyzbar.py
+++ lift/test_pyzbar.py
@@ -6,6 +6,14 @@ import requests
 from pyzbar.pyzbar import decode
 from PIL import Image
 
+"""
+二维码扫描出来的URL： http://117.25.179.165:7003/wapq/elevatorSearch.jsp?&EqpCod=T028592
+
+                      http://117.25.179.165:7003/wapq/elevatorSearch.jsp?&EqpCod=T008858
+
+真正电梯信息的URL：http://117.25.179.165:7003/wapq/elevator.do?task=search&eqpCod=T028592
+"""
+
 def get_html(url):
     try:
         r = requests.get(url, timeout=30)
@@ -16,9 +24,10 @@ def get_html(url):
         return "ERROR"
 
 
-decoders = decode(Image.open('lift1.png'))
+decoders = decode(Image.open('lift.png'))
 decoder = decoders.pop()
 url = decoder[0]
+print('QRCode Url = %s' % url)
 print(get_html(url))
 
 # 获取网页的内容

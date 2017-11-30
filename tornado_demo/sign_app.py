7c3f17ef2112def5b793a67346e62f3e6ace21c6
wfj <wfj@feibo.cn>
2017-12-01 07:04:27 +0800

    flask env

--- tornado_demo/sign_app.py
+++ tornado_demo/sign_app.py
@@ -7,11 +7,25 @@ class IndexPageHandler(web.RequestHandler):
             pass
 
         def get(self):
+            # self.write("Hello, world")
+            self.render('index.html')
+
+
+class CodeHandler(web.RequestHandler):
+        def data_received(self, chunk):
+            pass
+
+        def get(self):
             self.write("Hello, world")
+
+settings = {
+    'template_path': 'template'
+}
 
 application = web.Application([
     (r"/", IndexPageHandler),
-])
+    (r"/get_code", CodeHandler),
+],settings)
 
 if __name__ == '__main__':
     http_server = httpserver.HTTPServer(application)
